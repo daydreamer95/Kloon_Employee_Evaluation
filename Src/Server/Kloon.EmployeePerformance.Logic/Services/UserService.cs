35c4949d20846ee343752af48f41522702ec1775
Dương Hoàng <manchestervn1996@gmail.com>
2021-05-14 11:17:16 +0700

    Usermodel and Userservice format

---
+++ Src/Server/Kloon.EmployeePerformance.Logic/Services/UserService.cs
@@ -0,0 +1,62 @@
+﻿using Kloon.EmployeePerformance.Logic.Services.Base;
+using Kloon.EmployeePerformance.Models.Common;
+using Kloon.EmployeePerformance.Models.User;
+using Kloon.EmployeePerformance.Logic.Services.Base;
+using Kloon.EmployeePerformance.DataAccess.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kloon.EmployeePerformance.DataAccess;
+
+namespace Kloon.EmployeePerformance.Logic.Services
+{
+    public interface IUserService
+    {
+        ResultModel<List<UserModel>> GetAll(string searchText = "");
+        ResultModel<List<UserModel>> GetById(int userId);
+        ResultModel<UserModel> Create(UserModel userModel, string password);
+        ResultModel<UserModel> Update(UserModel userModel);
+        ResultModel<bool> Delete(int userId);
+    }
+    public class UserService : IUserService
+    {
+        private readonly IAuthenLogicService<UserService> _logicService;
+        private readonly IUnitOfWork<EmployeePerformanceContext> _dbContext;
+
+        private readonly IEntityRepository<User> _users;
+        public UserService(IAuthenLogicService<UserService> logicService)
+        {
+            _logicService = logicService;
+            _dbContext = logicService.DbContext;
+
+            _users = _dbContext.GetRepository<User>();
+        }
+
+        public ResultModel<UserModel> Create(UserModel userModel, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultModel<bool> Delete(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultModel<List<UserModel>> GetAll(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultModel<List<UserModel>> GetById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultModel<UserModel> Update(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Src/Server/Kloon.EmployeePerformance.Models/User/UserModel.cs
+++ Src/Server/Kloon.EmployeePerformance.Models/User/UserModel.cs
@@ -8,5 +8,15 @@ namespace Kloon.EmployeePerformance.Models.User
 {
     public class UserModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Position { get; set; }
+        public bool Gender { get; set; }
+        public DateTime Dob { get; set; }
+        public string PhoneNo { get; set; }
+        public int Role { get; set; }
     }
 }
