253cdc3916aa2cba374c68105a9001cbfea3878b
KLOONVN\pham.van.long <pham.van.long@kloon.vn>
2021-05-14 14:20:27 +0700

    update Usermodel

--- Src/Server/Kloon.EmployeePerformance.Models/User/UserModel.cs
+++ Src/Server/Kloon.EmployeePerformance.Models/User/UserModel.cs
@@ -9,14 +9,14 @@ namespace Kloon.EmployeePerformance.Models.User
     public class UserModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+     
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Position { get; set; }
-        public bool Gender { get; set; }
-        public DateTime Dob { get; set; }
+        public int PositionId { get; set; }
+        public bool Sex { get; set; }
+        public DateTime DoB { get; set; }
         public string PhoneNo { get; set; }
-        public int Role { get; set; }
+        public int RoleId { get; set; }
     }
 }
