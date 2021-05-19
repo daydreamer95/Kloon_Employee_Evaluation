4307c354b16636438f7c487c99c8f5095d523614
Dương Hoàng <manchestervn1996@gmail.com>
2021-05-19 17:53:46 +0700

    update user page ui

--- Src/Client/src/app/shared/components/user-form/user-form.component.ts
+++ Src/Client/src/app/shared/components/user-form/user-form.component.ts
@@ -24,6 +24,7 @@ import {
 import { UserModel } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services';
+import { CommonService } from '../../services/common.service';
 
 @Component({
   selector: 'app-user-form',
@@ -42,7 +43,7 @@ export class UserFormComponent implements OnInit {
   popupVisible = false;
   popupConfirmDeleteVisible = false;
   popupTitle = '';
-  buttonCloseTitle='Close';
+  buttonCloseTitle = 'Close';
   currUser: UserModel;
   titleChange: any;
   sexDataSource = [
@@ -57,7 +58,8 @@ export class UserFormComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private authService: AuthService
+    private authService: AuthService,
+    private common: CommonService
   ) {
     this.isAdminRole = this.authService.isRoleAdministrator;
   }
@@ -66,45 +68,42 @@ export class UserFormComponent implements OnInit {
     switch (this.model.state) {
       case FormState.CREATE:
         this.popupTitle = 'CREATE USER';
-        this.buttonCloseTitle = "Cancel"
+        this.buttonCloseTitle = 'Cancel';
         break;
       case FormState.DETAIL:
         this.popupTitle = 'DETAIL';
-        this.buttonCloseTitle = "Exit"
+        this.buttonCloseTitle = 'Exit';
         break;
       case FormState.EDIT:
         this.popupTitle = 'EDIT USER';
-        this.buttonCloseTitle = "Cancel"
+        this.buttonCloseTitle = 'Cancel';
         break;
     }
     this.popupVisible = true;
     this.currUser = this.model.data;
     // this.myform.instance._refresh();
     this.onTitleButtonChange();
-    this.titleChange._options._optionManager._options.toolbarItems[4].options.text = this.buttonCloseTitle;
-
+    this.titleChange._options._optionManager._options.toolbarItems[4].options.text =
+      this.buttonCloseTitle;
   }
-  
-  onContentReady(e){
 
+  onContentReady(e) {
     this.titleChange = e.component;
   }
 
-  onTitleButtonChange(){
-
-    if(this.model == null)
-      return this.buttonCloseTitle;
-      switch (this.model.state) {
-        case FormState.CREATE:
-          this.buttonCloseTitle = "Cancel"
-          break;
-        case FormState.DETAIL:
-          this.buttonCloseTitle = "Exit"
-          break;
-        case FormState.EDIT:
-          this.buttonCloseTitle = "Cancel"
-          break;
-      }
+  onTitleButtonChange() {
+    if (this.model == null) return this.buttonCloseTitle;
+    switch (this.model.state) {
+      case FormState.CREATE:
+        this.buttonCloseTitle = 'Cancel';
+        break;
+      case FormState.DETAIL:
+        this.buttonCloseTitle = 'Exit';
+        break;
+      case FormState.EDIT:
+        this.buttonCloseTitle = 'Cancel';
+        break;
+    }
     return this.buttonCloseTitle;
   }
 
@@ -114,7 +113,7 @@ export class UserFormComponent implements OnInit {
     icon: 'close',
     onClick: (e) => {
       this.popupVisible = false;
-    }
+    },
   };
 
   createButtonOptions = {
@@ -132,8 +131,14 @@ export class UserFormComponent implements OnInit {
           this.popupVisible = false;
           this.onRefreshGrid.emit();
         },
-        (error) => {
-          alert(error);
+        (err) => {
+          if (err.error === 'INVALID_MODEL_DUPLICATED_EMAIL') {
+            this.common.UI.multipleNotify(
+              'Email is existed !',
+              'error',
+              2000
+            );
+          }
         }
       );
     },
@@ -154,7 +159,9 @@ export class UserFormComponent implements OnInit {
 
     onClick: (e) => {
       this.model.state = FormState.EDIT;
-      this.titleChange._options._optionManager._options.toolbarItems.filter(t => t.options.icon == 'close')[0].options.text = "Cancel";
+      this.titleChange._options._optionManager._options.toolbarItems.filter(
+        (t) => t.options.icon == 'close'
+      )[0].options.text = 'Cancel';
       this.myform.instance._refresh();
       this.myform.instance.repaint();
     },
@@ -176,8 +183,14 @@ export class UserFormComponent implements OnInit {
           this.popupVisible = false;
           this.onRefreshGrid.emit();
         },
-        (error) => {
-
+        (e: any) => {
+          if (e.error) {
+            this.common.UI.multipleNotify(
+              'Email is existed !',
+              'error',
+              2000
+            );
+          }
         }
       );
     },
@@ -202,17 +215,13 @@ export class UserFormComponent implements OnInit {
           this.popupVisible = false;
           this.onRefreshGrid.emit();
         },
-        (error) => {
-
-        }
+        (error) => {}
       );
     },
   };
   ////#endregion
 
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 }
 
 @NgModule({
